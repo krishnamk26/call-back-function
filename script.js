e33443fe6cec3e334ade6678246ca948d900b389
krishnamk26 <127760812+krishnamk26@users.noreply.github.com>
2023-04-19 20:47:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var countdown=document.createElement("div");
+countdown.className="main";
+setTimeout(()=>{
+    countdown.innerHTML="10"
+    setTimeout(()=>{
+        countdown.innerText="9"; 
+        setTimeout(()=>{
+            countdown.innerText="8";
+            setTimeout(()=>{
+                countdown.innerText="7";
+                setTimeout(()=>{
+                    countdown.innerText="6"; 
+                    setTimeout(()=>{
+                        countdown.innerText="5"; 
+                        setTimeout(()=>{
+                            countdown.innerText="4"; 
+                            setTimeout(()=>{
+                                countdown.innerText="3"; 
+                                setTimeout(()=>{
+                                    countdown.innerText="2";
+                                    setTimeout(()=>{
+                                        countdown.innerText="1"; 
+                                        setTimeout(()=>{
+                                            countdown.innerText="Happy Independence Day"; 
+                                        },1000);  
+                                    },1000);   
+                                },1000);  
+                            },1000);  
+                        },1000);  
+                    },1000);  
+                },1000);  
+            },1000);   
+        },1000);  
+    },1000);  
+},1000)
+document.body.appendChild(countdown);   
